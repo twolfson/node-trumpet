5966530706bd952e99683b18e44f38be25ab20af
Todd Wolfson <todd@twolfson.com>
2013-11-28 02:29:22 -0800

    Discovered the `--tap` command. yey.

--- test/xml.js
+++ test/xml.js
@@ -3,20 +3,25 @@ var trumpet = require('../');
 var through = require('through');
 var concat = require('concat-stream');
 var fs = require('fs');
-var expected = fs.readFileSync(__dirname + '/xml_expected.html', 'utf8');
+var expected = fs.readFileSync(__dirname + '/loud_expected.html', 'utf8');
+
+console.log('hai');
 
 test('xml', function (t) {
     t.plan(1);
     var tr = trumpet();
 
-    // var xml = tr.select('*').createStream();
-    // xml.pipe(through(function (buf) {
-    //     this.queue(buf.toString().toUpperCase());
-    // })).pipe(xml);
+    var xml = tr.select('.load').createStream();
+    xml.pipe(through(function (buf) {
+        this.queue(buf.toString().toUpperCase());
+    })).pipe(xml);
 
-    fs.createReadStream(__dirname + '/xml.html')
+    console.log('hai');
+
+    fs.createReadStream(__dirname + '/loud.html')
         .pipe(tr)
         .pipe(concat(function (src) {
+            console.log(src.toString());
             t.equal(src.toString('utf8'), expected);
         }))
     ;
